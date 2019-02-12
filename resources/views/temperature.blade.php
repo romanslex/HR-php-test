cee587608b05c391bf25521c08bd45ae7ab6a180
Roman <romanslex@gmail.com>
2019-02-12 16:14:54 +0300

    Создал шаблон для страницы с температурой

---
+++ resources/views/temperature.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Страница температуры</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+<div class="card" style="width: 18rem;">
+    <div class="card-body">
+        <h5 class="card-title">Температура в Брянске: {{$temp}}°C</h5>
+    </div>
+</div>
+</body>
+</html>
