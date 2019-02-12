b6b7736bbc24048878423a16b6fb1ce5aaeb8318
Roman <romanslex@gmail.com>
2019-02-12 20:02:03 +0300

    Добавил разметку таблицы заказов

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -9,6 +9,32 @@
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-
+<div class="container">
+    <div class="row">
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Партнер</th>
+                <th scope="col">Стоимость</th>
+                <th scope="col">Состав заказа</th>
+                <th scope="col">Статус заказа</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($orders as $order)
+                <tr>
+                    <td>{{$order['id']}}</td>
+                    <td>{{$order['partnerName']}}</td>
+                    <td>{{$order['sum']}}</td>
+                    <td>{{$order['composition']}}</td>
+                    <th>{{$order['status']}}</th>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+        {{ $orders->links() }}
+    </div>
+</div>
 </body>
 </html>
