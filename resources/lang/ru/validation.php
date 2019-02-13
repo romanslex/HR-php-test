d017a3d10a820fa90ef9d014a4f23c387044bffe
Roman <romanslex@gmail.com>
2019-02-13 13:34:12 +0300

    Ошибки валидации на русском

---
+++ resources/lang/ru/validation.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'required' => 'Необходимо заполнить поле :attribute',
+    'email' => 'Некорректный email',
+    'in' => 'Выбран некорректный :attribute',
+    'exists' => 'Выбран некорректный :attribute',
+
+    'attributes' => [
+        "status" => "Статус",
+        "partner" => "Партнер",
+        "client_email" => "Email Клиента"
+    ],
+];
